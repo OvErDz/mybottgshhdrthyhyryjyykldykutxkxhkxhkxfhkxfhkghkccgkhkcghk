06a945afc168ed9b02c69839623a16c9d070069c
OvErDz <41547554+OvErDz@users.noreply.github.com>
2018-08-14 13:42:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
  const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const moment = require('moment');
+const jimp = require('jimp');
+const Canvas = require('canvas');
 client.on('ready', () => {
   client.user.setGame('=help|=inv','https://www.twitch.tv/OvErDz1');
   console.log('---------------');
